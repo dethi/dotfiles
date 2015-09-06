b3397fb26ea6ab5746332a7afc0b4bee0eb3f309
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-09-07 01:29:55 +0200

    fix issue with password requests

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ sudo echo "Thanks."
 if [ $(uname) = "Darwin" ]; then
     cecho "Setup Mac installation..."
 
-    # Homebrew
     echo | ruby -e "$(curl -fsSL $HOMEBREW)"
 
     brew update
@@ -39,26 +38,24 @@ sudo pip install --upgrade pip setuptools
 sudo pip install --upgrade flake8 virtualenv
 
 cecho "Clone dotfiles repository..."
-mkdir -p ~/Documents/Git
-git clone https://github.com/dethi/all.git $SCRIPTPATH
 (
+    mkdir -p ~/Documents/Git
+    cd ~/Documents/Git
+    git clone https://github.com/dethi/all.git
+
     # Little hack to clone the repository without my SSH key and then
     # reset origin to use SSH because I hate writing my username/password
-    cd $SCRIPTPATH
+    cd all
     git remote set-url origin git@github.com:dethi/all.git
 )
 
-# OhMyZsh
 cecho "Install OhMyZsh"
-(
-	TMP="/tmp/com.github.dethi.all.ohmyzsh-install.sh"
-	wget $OHMYZSH -O $TMP
-	
-	# Remove the last two lines, so the installation can continue
-	awk -v n=2 'NR>n{print line[NR%n]};{line[NR%n]=$0}' > $TMP
-	sh $TMP
-	rm -f ~/.zshrc
-)
+TMP="/tmp/com.github.dethi.all.ohmyzsh-install.sh"
+wget $OHMYZSH -O "$TMP.bak"
+# Remove the last two lines, so the installation can continue
+awk -v n=2 'NR>n{print line[NR%n]};{line[NR%n]=$0}' "$TMP.bak" > $TMP
+sh $TMP
+rm -f ~/.zshrc
 
 echo "Generate links..."
 ln -s "$SCRIPTPATH/dotfiles/.gitconfig" ~/.gitconfig
@@ -75,13 +72,11 @@ mkdir -p ~/.oh-my-zsh/themes
 ln -s "$SCRIPTPATH/.oh-my-zsh/themes/dethi.zsh-theme" \
     ~/.oh-my-zsh/themes/dethi.zsh-theme
 
-# Vundle
 cecho "Install Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 vim +GoInstallBinaries +qall
 
-# Configure YouCompleteMe
 cecho "Configure YouCompleteMe..."
 python ~/.vim/bundle/YouCompleteMe/install.py \
     --clang-completer --gocode-completer
